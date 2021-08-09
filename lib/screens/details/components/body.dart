65e650497e35eae1b5cc897cf818841881f83702
furqanalishah <syedfurqan@wanclouds.net>
2021-08-09 16:54:49 +0500

    add details page

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/details/components/image_and_icons.dart';
+import 'package:plant_app/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ImageAndIcons(),
-      ],
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 441),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Buy Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Description",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: kDefaultPadding * 2,
+          )
+        ],
+      ),
     );
   }
 }
